6ef9903f4bfafec1eb72d4316acb2cbac40d9fe7
Apoorva Raj <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-09-27 22:19:46 +0530

    Add console commands

---
+++ printing.js
@@ -0,0 +1,13 @@
+
+//some operations on browser console
+
+
+console.log("log printing");
+console.log("log printing2");
+
+process.stdout.write("changed") // another way of console log
+process.stdout.write("changed")
+
+console.table({"name": "city"}) //prints in table format in browser console
+
+console.warn({msg: "warning"}); // prints in color inside browser console
