59b84f1717b17c263e043158767d203decfab83d
Apoorva Raj <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-09-27 22:20:06 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,36 @@
+// Different types of datatypes in JS
+
+console.log("Hello"); // string datatype
+
+/*
+    string
+    Number
+    Boolean
+    Bigint
+
+
+    undefined --- something exists but don't know how to descibe it
+    null  --- nothing exists at all .i.e empty
+
+    objecy
+
+    symbol
+
+*/
+
+
+//some examples 
+
+let name = "code"
+let score = 102 
+let isLogedIn = true
+
+//object
+let teaTypes = ["string", "number", "boolean"];
+let  u = {firstname: "John", lastname: "doe"}
+
+
+// Overwrite values
+
+ let game = "Tombraider"; // "let" is used not const or it can't be changed then.
+ game = "Elden Ring"; 
