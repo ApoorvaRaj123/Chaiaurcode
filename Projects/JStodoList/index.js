42fd2557495f1f6aa13fcf8561c7a65eb50a06c8
Apoorva Raj <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-10-29 23:32:20 +0530

    Update index.js

--- Projects/JStodoList/index.js
+++ Projects/JStodoList/index.js
@@ -1,18 +1,51 @@
-const newTask = document.querySelector(".inputAdd");
-const addButton = document.querySelector(".inputBtn");
-const list = document.querySelector(".tasks ul");
+document.addEventListener("DOMContentLoaded", () => {
+  const newTask = document.querySelector(".inputAdd");
+  const addButton = document.querySelector(".inputBtn");
+  const list = document.querySelector(".tasks ul");
 
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-let tasks = [];
+  tasks.forEach((task) => renderTasks(task))
 
+  addButton.addEventListener("click", (e) => {
+    const taskText = newTask.value.trim();
+    if (taskText === "") return;
+    else {
+      const obj = {
+        id: Date.now(),
+        text: taskText,
+        completed: false,
+      };
 
-addButton.addEventListener("click", (e) => {
-  if (newTask.value === "") return;
-  else {
-    localStorage.setItem("task", newTask.value);
-    tasks.push(newTask.value);
+      tasks.push(obj);
+
+      save();
+
+      newTask.value = "";
+    }
+    // const task = e.target
+  });
+
+  function renderTasks(task) {
+    const li = document.createElement("li");
+    li.setAttribute("data-id", task.id);
+
+    if(task.completed) li.classList.add("completed");
+
+    li.innerHTML = `
+    <span>${task.text}</span>
+    <button>delete</button>
+    `;
+
+    li.addEventListener("click", (e) =>{
+      task.completed = !task.completed;
+      save();
+    });
+
+    list.appendChild(li)
   }
-  // const task = e.target
-});
 
-function renderTasks() {}
+  function save() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+});
